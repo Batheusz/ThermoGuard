12cbee9b9ca40382dfd42137210cec3a7ecc0c51
Matheus Henrique Orsini <65618285+Batheusz@users.noreply.github.com>
2025-04-21 15:15:57 -0300

    Testando Blynk
    Envio de uma mensagem para o Blynk

--- src/main.cpp
+++ src/main.cpp
@@ -1,25 +1,42 @@
 #include <Arduino.h>
 #include <WiFi.h>
+#include <WiFiClient.h>
 #include <HTTPClient.h>
 #include <UrlEncode.h>
+
+// Blynk config
+#define BLYNK_TEMPLATE_ID "TMPL2KunrIRsv"
+#define BLYNK_TEMPLATE_NAME "ThermoGuard"
+#define BLYNK_AUTH_TOKEN "oXNoqXQ6U49cD_OIWfaX756gTmnXg3Bx"
+#define BLYNK_PRINT Serial // Debuging blynk on serial
+#include <BlynkSimpleEsp32.h>
 
 // Whatsapp API key
 String numCelular = "";
 String apiWhats = "";
 
 // WiFi configuração
-const char* nomeWifi =  "Vazio";
-const char* senhaWifi = "Vazio";
+const char* nomeWifi =  "";
+const char* senhaWifi = "";
 // Funções
 uint8_t EnviarWhats(String messagem);
 uint8_t ConectaWifi(const char* nome, const char* senha);
 
 void setup() {
   Serial.begin(115200);
+  ConectaWifi(nomeWifi,senhaWifi);
 
+  Blynk.config(BLYNK_AUTH_TOKEN);
+  Blynk.connect();
+  while (!Blynk.connected()) {
+    delay(100);
+    Serial.print("#");
+  }
+  Blynk.virtualWrite(V0, 12.25);
   
+  // WiFi.disconnect(true);
+  // WiFi.mode(WIFI_OFF);
   
-  EnviarWhats("ALERTA!! Verifique o paciente!");
 }
 
 void loop() {
@@ -38,28 +55,20 @@ void loop() {
  *       - 0 se houve falha no envio.
  */
 uint8_t EnviarWhats(String messagem) {
+  String url = "https://api.callmebot.com/whatsapp.php?phone=" + numCelular + "&apikey=" + apiWhats + "&text=" + urlEncode(messagem);
+  HTTPClient http;
+  http.begin(url);
+  http.addHeader("Content-Type", "application/x-www-form-urlencoded");
+  int httpResponseCode = http.POST(url);
   
-  uint8_t checkWifi = ConectaWifi(nomeWifi, senhaWifi);
-  if (checkWifi == 0) {
-    return 2;
+  if (httpResponseCode == 200) {
+    http.end();
+    return 1;
   }
   else {
-    String url = "https://api.callmebot.com/whatsapp.php?phone=" + numCelular + "&apikey=" + apiWhats + "&text=" + urlEncode(messagem);
-    HTTPClient http;
-    http.begin(url);
-    http.addHeader("Content-Type", "application/x-www-form-urlencoded");
-    int httpResponseCode = http.POST(url);
-    
-    if (httpResponseCode == 200) {
-      http.end();
-      return 1;
-    }
-    else {
-      http.end();
-      return 0;
-    }
+    http.end();
+    return 0;
   }
-  
 }
 
 /**
